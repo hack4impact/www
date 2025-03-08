1821ed54cf43ae2e3655633a0c19bd564786303c
khoa-l <kly27@calpoly.edu>
2025-03-07 20:43:42 -0800

    feat: added Card component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,9 @@ import Container from '@/components/Container';
 import Hero from '@/components/Hero';
 import StaggerAnimOnScroll from '@/components/StaggerAnimOnScroll';
 import { StaggerItem } from '@/components/StaggerAnimOnScroll';
+import Card from '@/components/Card';
+
+import { FaNetworkWired, FaHandshake, FaFolder } from 'react-icons/fa6';
 
 // type Props = {
 //   featuredProjects: Project[];
@@ -15,49 +18,49 @@ export default function Home() {
       <Hero />
       <Container>
         <h2>Our Initiatives</h2>
-        {/* <StaggerAnimOnScroll className={styles.card_row}> */}
-        {/* <StaggerItem> */}
-        {/*       <Card */}
-        {/*         title="Chapter Network" */}
-        {/*         subtitle="We mentor" */}
-        {/*         description="We help new chapters help their local community by providing established communication channels with veteran chapters and mentors" */}
-        {/*         href="/work#our-chapters" */}
-        {/*         buttonTitle="Current Chapters"> */}
-        {/*         <FaNetworkWired /> */}
-        {/*       </Card> */}
-        {/* </StaggerItem> */}
-        {/* <StaggerItem> */}
-        {/*       <Card */}
-        {/*         title="Nonprofit Projects" */}
-        {/*         subtitle="We build" */}
-        {/*         description="We help nonprofits do the good they are meant to be doing more efficiently. By building them the custom software they need, they can focus on their important work" */}
-        {/*         href="/work" */}
-        {/*         buttonTitle="Current Projects"> */}
-        {/*         <FaHandshake size={34} /> */}
-        {/*       </Card> */}
-        {/* </StaggerItem> */}
-        {/* <StaggerItem> */}
-        {/*       <Card */}
-        {/*         title="Chapter Resources" */}
-        {/*         subtitle="We share" */}
-        {/*         description="Our 6 years of experience in building socially impactful technology has given our chapters and members the tools to help nonprofits" */}
-        {/*         href="https://www.notion.so/h4i/Hack4Impact-Home-a8145b7695d64f04bca8acb5094d5c83" */}
-        {/*         isExternalLink */}
-        {/*         buttonTitle="Our Resources"> */}
-        {/*         <FaFolder /> */}
-        {/*       </Card> */}
-        {/* </StaggerItem> */}
-        {/* </StaggerAnimOnScroll> */}
+        <StaggerAnimOnScroll className={styles.card_row}>
+          <StaggerItem>
+            <Card
+              title="Chapter Network"
+              subtitle="We mentor"
+              description="We help new chapters help their local community by providing established communication channels with veteran chapters and mentors"
+              href="/work#our-chapters"
+              buttonTitle="Current Chapters">
+              <FaNetworkWired />
+            </Card>
+          </StaggerItem>
+          <StaggerItem>
+            <Card
+              title="Nonprofit Projects"
+              subtitle="We build"
+              description="We help nonprofits do the good they are meant to be doing more efficiently. By building them the custom software they need, they can focus on their important work"
+              href="/work"
+              buttonTitle="Current Projects">
+              <FaHandshake size={34} />
+            </Card>
+          </StaggerItem>
+          <StaggerItem>
+            <Card
+              title="Chapter Resources"
+              subtitle="We share"
+              description="Our 6 years of experience in building socially impactful technology has given our chapters and members the tools to help nonprofits"
+              href="https://www.notion.so/h4i/Hack4Impact-Home-a8145b7695d64f04bca8acb5094d5c83"
+              isExternalLink
+              buttonTitle="Our Resources">
+              <FaFolder />
+            </Card>
+          </StaggerItem>
+        </StaggerAnimOnScroll>
       </Container>
       <Container>
         {/*   <h2>Featured Projects</h2> */}
-        // <StaggerAnimOnScroll>
-          {/*     {featuredProjects.map(item => ( */}
-          {/* <StaggerItem key={item.name}> */}
-          {/*         <ProjectView {...item} /> */}
-          {/* </StaggerItem> */}
-          {/*     ))} */}
-        // </StaggerAnimOnScroll>
+        {/* <StaggerAnimOnScroll> */}
+        {/*     {featuredProjects.map(item => ( */}
+        {/* <StaggerItem key={item.name}> */}
+        {/*         <ProjectView {...item} /> */}
+        {/* </StaggerItem> */}
+        {/*     ))} */}
+        {/* </StaggerAnimOnScroll> */}
         <div className={styles.button_row}>
           <LinkButton href="/work#our-chapters">See all chapters</LinkButton>
           <LinkButton href="/apply/nonprofit" type="secondary">
