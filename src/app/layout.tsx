51e61f5f8ca79142cf3598774fadfae41f5d05c9
khoa-l <kly27@calpoly.edu>
2026-02-01 03:42:34 -0800

    refactor: moved fonts to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,42 @@
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
 import './globals.css'
+
+const inclusiveSans = localFont({
+  src: [
+    {
+      path: '../fonts/InclusiveSans.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/InclusiveSans-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-inclusive-sans',
+})
+
+const newsreader = localFont({
+  src: [
+    {
+      path: '../fonts/Newsreader.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Newsreader-Italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-newsreader',
+})
 
 export const metadata: Metadata = {
   title: {
@@ -21,8 +56,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className='min-h-screen flex flex-col relative'>
+    <html
+      lang='en'
+      className={`${inclusiveSans.variable} ${newsreader.variable}`}
+    >
+      <body className='flex min-h-screen flex-col relative'>
         <Header />
         <main className='flex-1'>{children}</main>
         <Footer />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inclusive-sans)'],
+        serif: ['var(--font-newsreader)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
